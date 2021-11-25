5de2faeac55b219a58d25399ba4c05b81595b96e
Scott Provence <scott.provence@yahoo.com>
2021-11-24 19:01:56 -0500

    Implement aliases for components, config and utils

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -3,8 +3,8 @@ import path from "path";
 import matter from "gray-matter";
 import { marked } from "marked";
 import Link from "next/link";
-import Layout from "../../components/Layout";
-import CategoryLabel from "../../components/CategoryLabel";
+import Layout from "@/components/Layout";
+import CategoryLabel from "@/components/CategoryLabel";
 
 export default function PostPage({ frontmatter, content, slug }) {
   return (
