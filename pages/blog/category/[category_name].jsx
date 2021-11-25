c3811366fd803ca86e42bca8f1c25bac80b76b77
Scott Provence <scott.provence@yahoo.com>
2021-11-24 20:20:56 -0500

    Refactor getStaticProps for fetching posts

--- pages/blog/category/[category_name].jsx
+++ pages/blog/category/[category_name].jsx
@@ -1,10 +1,9 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import Link from "next/link";
 import Layout from "@/components/Layout";
 import Post from "@/components/Post";
-import { sortByDate } from "@/utils/index";
+import { getPosts } from "@/lib/posts";
 
 export default function CategoryPage({ posts }) {
   return (
@@ -46,29 +45,13 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { category_name } }) {
-  const files = fs.readdirSync(path.join("posts"));
-
-  const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "");
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+  const posts = getPosts();
 
   const categoryPosts = posts.filter(
     (post) => post.frontmatter.category.toLowerCase() === category_name
   );
 
   return {
-    props: { posts: categoryPosts.sort(sortByDate) },
+    props: { posts: categoryPosts },
   };
 }
